1a23c902129e4576f2869e0c5cbc5d6fdb104587
mikdevio <villacis.mhva@gmail.com>
2026-03-13 21:11:16 -0500

    Added: search input group

--- weather-dashboard/src/components/Dashboard.tsx
+++ weather-dashboard/src/components/Dashboard.tsx
@@ -1,3 +1,7 @@
+import { MagnifyingGlassCircleIcon } from "@heroicons/react/24/outline"
+
+
+
 interface DashboardProps {
     title: string
 }
@@ -14,12 +18,15 @@ const Dashboard: React.FC<DashboardProps> = ({ title }) => {
             <main>
                 <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
                     {/* Cambiado text-sm/6 por text-sm y leading-6 para mayor compatibilidad */}
-                    <label className="block text-sm leading-6 font-medium text-white">Price</label>
+                    <label className="block text-2xl leading-6 font-bold text-white">Search a place:</label>
                     <div className="mt-2">
-
-                        <div className="flex items-center rounded-md bg-white/5 pl-3 outline-white/10">
-                            <span className="text-white">Contenido de prueba...</span>
+                        <div className="flex items-center rounded-md pl-0 outline-1 -outline-offset-1 outline-gray-600 has-[input:focus-within]:outline-2 has-[input:focus-within]:-outline-offset-2 has-[input:focus-within]:outline-indigo-500">
+                            <input type="text" name="search-box" id="search-box" className="block min-w-0 grow bg-gray-800 py-1.5 pr-3 pl-3 text-base text-white placeholder:text-gray-500 focus:outline-none sm:text-base/8" />
+                            <div className="grid shrink-0 grid-cols-1 focus-within:relative  bg-auto pr-1 pl-1">
+                                <MagnifyingGlassCircleIcon className="size-8 text-white h-full " />
+                            </div>
                         </div>
+
                     </div>
                 </div>
             </main>
