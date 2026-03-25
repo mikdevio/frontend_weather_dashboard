f57bb767c3aed798d4654b5028ca0ad997cc7a4f
mikdevio <villacis.mhva@gmail.com>
2026-03-25 10:48:52 -0500

    Added axios api class and fetch procedure to WeatherBox component

--- weather-dashboard/src/components/WeatherBox.tsx
+++ weather-dashboard/src/components/WeatherBox.tsx
@@ -1,13 +1,48 @@
+import { useEffect, useState } from "react";
+import api from "../services/ApiService";
 
 interface WeatherBoxProps {
     prop_1: string
 }
 
 const WeatherBox: React.FC<WeatherBoxProps> = ({ prop_1 }) => {
+    const [weather, setWeather] = useState<any>(null);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        const fetchWeather = async () => {
+            try {
+                setLoading(true);
+                const data = await api.getCurrentWeather("Quito");
+                setWeather(data);
+            } catch (error) {
+                setError("Cannot access to weather data");
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchWeather();
+    }, [])
+
+    if (loading) return <p className="text-white text-2xl text-center pt-2">Loading weather data...</p>
+    if (error) return <p className="text-red-500 text-2xl text-center pt-2">{error}</p>;
+
     return (
 
         <div className="bg-gray-900 py-24 sm:py-10 border-2 border-gray-600 mx-8 rounded-md">
-            <div className="min-h-dvh"></div>
+            <div className="min-h-dvh">
+                {weather && (
+                    <div className="card">
+                        <h1>{weather.name}</h1>
+                        <p>Temperatura: {Math.round(weather.main.temp)}°C</p>
+                        <p>Humedad: {weather.main.humidity}%</p>
+                        <p>Sensación: {Math.round(weather.main.feels_like)}°C</p>
+                        <p>Viento: {weather.wind.speed} m/s</p>
+                    </div>
+                )}
+            </div>
             <div className="mx-auto max-w-7xl px-6 lg:px-8">
                 <dl className="grid grid-cols-1 gap-x-8 gap-y-16 text-center lg:grid-cols-3">
                     <div className="mx-auto flex max-w-xs flex-col gap-y-4">
--- weather-dashboard/src/services/ApiService.ts
+++ weather-dashboard/src/services/ApiService.ts
@@ -1,16 +1,46 @@
-import axios from "axios";
+import axios, { Axios } from "axios";
 
 const apiKey = import.meta.env.VITE_OPENWEATHER_API_KEY;
 const baseURL = import.meta.env.VITE_OPENWEATHER_API_URL;
 
+class ApiService {
 
-const api = axios.create({
-    baseURL: baseURL,
-    params: {
-        appid: apiKey,
-        units: 'metric',
-        lang: 'es'
+    baseUrl: string;
+    apiKey: string;
+    units: string;
+    lang: string;
+    private api: any;
+
+    constructor(baseUrl: string, apiKey: string, units: string = 'metric', lang: string = 'es') {
+        this.baseUrl = baseUrl;
+        this.apiKey = apiKey;
+        this.units = units;
+        this.lang = lang;
+        this.createConnection();
     }
-})
+
+    async createConnection(): Promise<void> {
+        this.api = axios.create({
+            baseURL: this.baseUrl,
+            params: {
+                appid: this.apiKey,
+                units: this.units,
+                lang: this.lang
+            }
+        })
+    }
+
+    async getCurrentWeather(city: string) {
+        try {
+            const response = await this.api.get("/weather", { params: { q: city } });
+            return response.data;
+        } catch (error) {
+            console.error("Request error: ", error);
+            throw error;
+        }
+    }
+}
+
+let api = new ApiService(baseURL, apiKey)
 
 export default api;
